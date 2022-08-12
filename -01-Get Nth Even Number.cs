c5939142597cdeb975c190f96eccbb4901bf0157
Sameeh-Hamdan-CSharp <111115275+Sameeh-Hamdan-CSharp@users.noreply.github.com>
2022-08-12 11:17:07 +0300

    Update -01-Get Nth Even Number.cs

--- -01-Get Nth Even Number.cs
+++ -01-Get Nth Even Number.cs
@@ -1,12 +1,14 @@
-# Return the Nth Even Number
+//Return the Nth Even Number
 
-Example(Input --> Output)
+/*Example(Input --> Output)
 
 * 1 --> 0 (the first even number is 0)
 * 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
 * 100 --> 198
 * 1298734 --> 2597466
-
-## Solution:
-
-public static int NthEven(int n) => 2*(n-1);
+*/
+//Solution:
+public class Kata
+{
+  public static int NthEven(int n) => 2*(n-1);
+}
